4f9d43bbe358e0ece001f6f4f1ca1aed74328085
mstrojek@student.agh.edu.pl <mstrojek@student.agh.edu.pl>
2024-01-09 18:00:22 +0100

    zmienienie na float + dodanie żeby zwracało mi tylko odległości

--- Somsiad.py
+++ Somsiad.py
@@ -20,13 +20,13 @@ def nearest_neighbor_tsp(distance_matrix, start_index):
         # Znalezienie indeksu najbliższego miasta
         next_index = np.argmin(distance_matrix[visited[-1], remaining_indices])
         next_city = remaining_indices[next_index]
-        
+
         # Aktualizacja całkowitej odległości
         total_distance += distance_matrix[visited[-1], next_city]
 
         # Dodanie miasta do listy odwiedzonych
         visited.append(next_city)
-    total_distance += distance_matrix[visited[-1],visited[0]]
+    total_distance += distance_matrix[visited[-1], visited[0]]
     visited.append(start_index)
 
     return total_distance, visited
@@ -36,19 +36,25 @@ def FindMin(distance_matrix):
     listOfResult = []
     for i in range(distance_matrix.shape[0]):
         listOfResult.append(nearest_neighbor_tsp(distance_matrix, i))
-    #return listOfResult   
+    # return listOfResult
     return list(filter(lambda x: x[0] == minValueInList(listOfResult), listOfResult))
-        
+
+def FindMinForPlots(distance_matrix):
+    listOfResult = []
+    for i in range(distance_matrix.shape[0]):
+        listOfResult.append(nearest_neighbor_tsp(distance_matrix, i)[0])
+    return listOfResult
+    #return list(filter(lambda x: x[0] == minValueInList(listOfResult), listOfResult))
 
 def minValueInList(listOfResult):
     return min(listOfResult, key=lambda x: x[0])[0]
-    
 
-#distance_matrix = np.random.randint(1, 100, size=(100, 100))  # Losowe odległości między miastami
-#np.fill_diagonal(distance_matrix, 0)
-#readData=pd.read_csv("Dane_TSP_127.csv",sep=";")
-readData=pd.read_csv("Dane_TSP_48.csv",sep=";", decimal=",")
-distance_matrix = readData.iloc[:,1:].astype(int).to_numpy()
 
+# distance_matrix = np.random.randint(1, 100, size=(100, 100))  # Losowe odległości między miastami
+# np.fill_diagonal(distance_matrix, 0)
+# readData=pd.read_csv("Dane_TSP_127.csv",sep=";")
+readData = pd.read_csv("Dane_TSP_48.csv", sep=";", decimal=",")
+
+distance_matrix = readData.iloc[:, 1:].astype(float).to_numpy()
 
 print(FindMin(distance_matrix))
